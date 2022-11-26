37216bfde04f8a051feeadbedc2fcd31b3b59701
Kai <kiamehrkhademi2019@gmail.com>
2022-11-26 12:38:14 -0800

    Started the login page.

--- android/app/src/main/java/com/example/pave/MainActivity.java
+++ android/app/src/main/java/com/example/pave/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.pave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button sin = (Button) findViewById(R.id.button);
+        sin.setBackgroundColor(Color.rgb(26,28,31));
+        Button sup = (Button) findViewById(R.id.button2);
+        sup.setBackgroundColor(Color.rgb(26,28,31));
+
     }
 }
