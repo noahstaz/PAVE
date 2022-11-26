8fb0df9f626ac5764f9466dbd418a5a5abf2bc4c
Noah Stasuik <noah.stasuik@gmail.com>
2022-11-26 14:39:29 -0800

    Work on splash-Page on going

---
+++ android/app/src/main/java/com/example/pave/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.pave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SplashActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+
+                try {
+                    sleep(2500);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                }
+            }
+        };
+        thread.start();
+    }
+
+}
