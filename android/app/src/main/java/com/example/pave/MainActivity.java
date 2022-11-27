18f63719b0ebeef4ca38d45ad8566e46326af05b
Kai <kiamehrkhademi2019@gmail.com>
2022-11-26 20:31:05 -0800

    Tried to save the user pass.

--- android/app/src/main/java/com/example/pave/MainActivity.java
+++ android/app/src/main/java/com/example/pave/MainActivity.java
@@ -2,32 +2,45 @@ package com.example.pave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sp = getSharedPreferences("MySharedPref", MODE_PRIVATE);
         Button sin = (Button) findViewById(R.id.button);
-        sin.setBackgroundColor(Color.rgb(26,28,31));
         Button sup = (Button) findViewById(R.id.button2);
-        sup.setBackgroundColor(Color.rgb(26,28,31));
+        EditText txtname = findViewById(R.id.editTextTextEmailAddress2);
+        EditText passname = findViewById(R.id.editTextTextPassword);
         sin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText txtname = findViewById(R.id.editTextTextEmailAddress2);
                 String uname = txtname.getText().toString();
-                Intent myin = new Intent(MainActivity.this, EnteredPage.class);
-                myin.putExtra("EXTRA_SESSION_ID", uname);
-                startActivity(myin);
+                String pass = passname.getText().toString();
+                String retrive = sp.getString(uname, "");
+                if(retrive.equals(pass)) {
+                    Intent myin = new Intent(MainActivity.this, EnteredPage.class);
+                    myin.putExtra("EXTRA_SESSION_ID", uname);
+                    startActivity(myin);
+                }
+                else{
+                    TextView errormsg = findViewById(R.id.textView4);
+                    errormsg.setTextColor(Color.parseColor("#FF0000"));
+                }
+
             }
         });
         sup.setOnClickListener(new View.OnClickListener() {
