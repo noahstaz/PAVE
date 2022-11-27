78e88c230708c2045fa49b9c9d5f93cdca92b374
Kai <kiamehrkhademi2019@gmail.com>
2022-11-26 23:24:36 -0800

    Added the link to the website.

--- android/app/src/main/java/com/example/pave/SignUp.java
+++ android/app/src/main/java/com/example/pave/SignUp.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 import java.io.FileNotFoundException;
 
@@ -25,6 +28,8 @@ public class SignUp extends AppCompatActivity {
         EditText txtname2 = findViewById(R.id.editTextTextEmailAddress3);
         EditText passname = findViewById(R.id.editTextTextPassword2);
         EditText passname2 = findViewById(R.id.editTextTextPassword3);
+        TextView hyperText = findViewById(R.id.textView12);
+        hyperText.setMovementMethod(LinkMovementMethod.getInstance());
         SharedPreferences.Editor editor = sp.edit();
         sup.setOnClickListener(new View.OnClickListener() {
             @Override
