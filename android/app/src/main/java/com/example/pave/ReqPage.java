5eacb8ed97e84c64b821cd4eb71ad8f719067fda
Kai <kiamehrkhademi2019@gmail.com>
2022-11-27 13:52:31 -0800

    Fixed final bugs.

--- android/app/src/main/java/com/example/pave/ReqPage.java
+++ android/app/src/main/java/com/example/pave/ReqPage.java
@@ -1,6 +1,7 @@
 package com.example.pave;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -21,6 +22,17 @@ public class ReqPage extends AppCompatActivity {
                 Intent myin = new Intent();
                 myin.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivity(myin);
+            }
+        });
+        Button linkBtn = (Button) findViewById(R.id.button6);
+        linkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.pave-p2p.tech/"));
+                startActivity(intent);
             }
         });
     }
