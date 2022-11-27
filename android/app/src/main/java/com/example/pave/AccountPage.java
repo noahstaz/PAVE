5ca1636245ebd1fd260459fdb70cf610af82faca
Kai <kiamehrkhademi2019@gmail.com>
2022-11-27 01:17:15 -0800

    Made the account page.

--- android/app/src/main/java/com/example/pave/AccountPage.java
+++ android/app/src/main/java/com/example/pave/AccountPage.java
@@ -1,6 +1,7 @@
 package com.example.pave;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,21 +9,19 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class EnteredPage extends AppCompatActivity {
-
+public class AccountPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_page);
-        TextView myTextView = (TextView) findViewById(R.id.textView8);
+        setContentView(R.layout.account_page);
+        TextView myTextView = (TextView) findViewById(R.id.textView15);
         String uname = getIntent().getStringExtra("EXTRA_SESSION_ID");
-        myTextView.setText("Username: " + uname);
-        Button bck = (Button) findViewById(R.id.button7);
+        myTextView.setText("Hello " + uname + "!");
+        Button bck = (Button) findViewById(R.id.button5);
         bck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myin = new Intent(EnteredPage.this, AccountPage.class);
-                myin.putExtra("EXTRA_SESSION_ID", uname);
+                Intent myin = new Intent(AccountPage.this, EnteredPage.class);
                 startActivity(myin);
             }
         });
