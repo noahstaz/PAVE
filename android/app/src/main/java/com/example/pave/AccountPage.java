dce1cbef3ca59984a3161782b3ea4a6618b606d2
Kai <kiamehrkhademi2019@gmail.com>
2022-11-27 02:18:08 -0800

    Made the account page.

---
+++ android/app/src/main/java/com/example/pave/AccountPage.java
@@ -0,0 +1,29 @@
+package com.example.pave;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account_page);
+        TextView myTextView = (TextView) findViewById(R.id.textView15);
+        String uname = getIntent().getStringExtra("EXTRA_SESSION_ID");
+        myTextView.setText("Hello " + uname + "!");
+        Button bck = (Button) findViewById(R.id.button5);
+        bck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myin = new Intent(AccountPage.this, EnteredPage.class);
+                startActivity(myin);
+            }
+        });
+    }
+}
---
+++ android/app/src/main/java/com/example/pave/MovePage.java
@@ -0,0 +1,14 @@
+package com.example.pave;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MovePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.move_page);
+    }
+}
